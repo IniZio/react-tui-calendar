11347ac9e33f0fd45468e6d46b50bd92506800bf
inizio <digit4free@gmail.com>
2018-07-14 22:33:31 +0800

    fix(globals): resolves tui-calendar globals in iife

--- bili.config.js
+++ bili.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   moduleName: 'TuiCalendar',
-  format: ['cjs', 'cjs-min', 'umd', 'umd-min', 'es', 'es-min', 'iife'],
+  format: ['cjs', 'cjs-min', 'umd', 'umd-min', 'es', 'es-min', 'iife', 'iife-min'],
   inline: false,
+  external: ['react', 'tui-calendar'],
   globals: {
     'react': 'React',
-    'tui-calendar': 'BaseCalendar'
+    'tui-calendar': 'tui.Calendar'
   }
 }
